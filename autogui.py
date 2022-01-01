2e86dc0c78361bcd4cd79a22a944decf9f446c1f
Rodolpho Pontes <rpgaudioso@gmail.com>
2022-01-01 17:02:41 -0300

    e

--- autogui.py
+++ autogui.py
@@ -2,14 +2,21 @@ import pyautogui
 from time import sleep
 
 
-def findBtAndClick(name):
-    pt = pyautogui.locateOnScreen('{}.png'.format(name))
-    print(pt)
-    if(pt):
-        ptCenter = pyautogui.center(pt)
-        pyautogui.click(ptCenter)
+# def findBtAndClick(name):
+#     pt = pyautogui.locateOnScreen('{}.png'.format(name))
+#     print(pt)
+#     if(pt):
+#         ptCenter = pyautogui.center(pt)
+#         pyautogui.click(ptCenter)
 
 
-findBtAndClick('connectWallet')
-sleep(10)
-findBtAndClick('signIn')
+# findBtAndClick('connectWallet')
+# sleep(10)
+# findBtAndClick('signIn')
+
+
+
+
+pyautogui.screenshot('map.png', region=(0,0, 2560, 1080))
+# pt = list(pyautogui.locateAllOnScreen('rock.png'))
+# print(pt)
