b270500572a3998f3933f2727ba1d93c70d6f1ee
Rodolpho Pontes <rpgaudioso@gmail.com>
2022-01-01 18:54:16 -0300

    objs

--- autogui.py
+++ autogui.py
@@ -1,23 +1,35 @@
 import pyautogui
+import mouse
 from time import sleep
 
-
-sleep(6)
-pyautogui.screenshot('map_current2.png', region=(0,0, 2560, 1080))
-
-pt = list(pyautogui.locateAllOnScreen('wall3.png'))
-print(len(pt))
-
-print('rock')
-
-pt = list(pyautogui.locateAllOnScreen('rock3.png' ))
-print(len(pt))
+# ---------------------------------------------------
+# Methods
+# ---------------------------------------------------
+def getObjList(type, confidence=0.94):
+    return list(pyautogui.locateAllOnScreen('objs/{}.png'.format(type), confidence=confidence))
 
 
-# 104
-# rock
-# 68
+walls = getObjList('wall')
+print('walls: {}'.format(len(walls)))
 
-# 106
-# rock
-# 46
+rocks = getObjList('rock')
+print('rocks: {}'.format(len(rocks)))
+
+woods =  getObjList('wood', 0.9)
+print('woods: {}'.format(len(woods)))
+
+irons =  getObjList('iron')
+print('irons: {}'.format(len(irons)))
+
+# golds =  getObjList('gold')
+# print('golds: {}'.format(len(golds)))
+
+# crystals =  getObjList('crystal')
+# print('crystals: {}'.format(len(crystals)))
+
+# jails =  getObjList('jail')
+# print('jails: {}'.format(len(jails)))
+
+for item in woods:
+    mouse.move((item.left+(item.width/2)), (item.top+(item.height/2)), True, 0.3)
+    sleep(0.4)
